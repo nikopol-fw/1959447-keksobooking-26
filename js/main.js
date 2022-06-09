1fd0efb7c5917094e41e35715fb366f2cd6bc582
Elena <pumpkinpum@gmail.com>
2022-06-09 13:09:50 +0300

    Внесены правки

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-let randomNumber;
+/**
+ * Функция для получения случайного числа
+ * https://javascript.ru/Math.Random
+ * @param {number} min - минимальное значение диапазона;
+ * @param {number} max - максимальное значение диапазона;
+ * @param {number} grade - количество знаков после запятой;
+ * @returns {number}
+ */
 
-function getRandomNumber(min, max, grade) {
-  if (min >= 0 && max >= 0 && min < max) {
-    randomNumber = Math.random() * (max - min) + min;
-    randomNumber = randomNumber.toFixed(grade);
-    return randomNumber;
-  } else {
-    alert('Неверное значение диапазона.');
+//todo Не добавлена проверка на отрицательное значение grade, тк есть в самом методе to.Fixed
+
+const getRandomNumber = (min, max, grade) => {
+  if (min < 0 || max < 0 || min > max) {
+    throw new Error('Неверное значение диапазона.');
   }
-}
-getRandomNumber(50, 100, 8);
+
+  let randomNumber = Math.random() * (max - min) + min;
+  randomNumber = +randomNumber.toFixed(grade);
+  return randomNumber;
+};
+
+const generatedNumber = getRandomNumber(50, 500, 4);
+//todo служебное сообщение
+// eslint-disable-next-line no-console
+console.log(generatedNumber);
+
