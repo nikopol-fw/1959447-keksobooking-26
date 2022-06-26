49d5f666a3a3ab4b85c4418bbcaea62edac06141
Elena <pumpkinpum@gmail.com>
2022-06-26 20:04:21 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ import {
 
 // eslint-disable-next-line no-console
 console.log(similarAdvertisement);
+
+
+//отдельный модуль для генерации похожих объявлений - popup.js
+
+//разметка похожих объявлений отображать в блоке, где должна быть карта.
+
+// шаблон #card
+
+const similarListElement = userDialog.querySelector('.setup-similar-list');
