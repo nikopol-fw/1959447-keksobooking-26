f16f5e9f7a6e2c7a7496cf507e9da2413d747f1f
Elena <pumpkinpum@gmail.com>
2022-06-28 13:50:45 +0300

    Task 7.15 - заполнение шаблона

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {
-  similarAdvertisement,
-  generateAdvertisement
+  similarAdvertisement
 } from './createadvertisement.js';
 
 // eslint-disable-next-line no-console
@@ -10,13 +9,13 @@ import {
 //отдельный модуль для генерации похожих объявлений - popup.js
 
 //создадим коллекция, ключ-значение.
-const typeHabitations = new Map();
-typeHabitations.set = ('flat', 'Квартира');
-typeHabitations.set = ('bungalow', 'Бунгало');
-typeHabitations.set = ('house', 'Дом');
-typeHabitations.set = ('palace', 'Дворец');
-typeHabitations.set = ('hotel', 'Отель');
-
+const typeHabitation = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
 
 //разметка похожих объявлений временно отображать в блоке, где должна быть карта.
 const insertAdvertisement = document.querySelector('#map-canvas');
@@ -27,42 +26,43 @@ const elementTemplate = document.querySelector('#card').content.querySelector('.
 //вызываем функцию генерации данных
 const similarElements = similarAdvertisement();
 
-generateAdvertisement();
+similarElements.forEach((advertisement) => {
 
-similarElements.forEach(() => {
   //клонирование шаблона
   const cardElement = elementTemplate.cloneNode(true);
   //вставляем данные
   const popupTitle = cardElement.querySelector('.popup__title');
-  popupTitle.textContent = offer.title;
+  popupTitle.textContent = advertisement.offer.title;
 
   const popupAddress = cardElement.querySelector('.popup__text--address');
-  popupAddress.textContent = offer.address;
+  popupAddress.textContent = advertisement.offer.address;
 
-  //строкой вида {{offer.price}} ₽/ночь. Например, «5200 ₽/ночь».
   const popupPrice = cardElement.querySelector('.popup__text--price');
-  popupPrice.textContent = '${offer.price}₽/ночь';
+  popupPrice.textContent = `${advertisement.offer.price} ₽/ночь`;
 
   const popupType = cardElement.querySelector('.popup__type');
-  popupType.textContent = typeHabitations.get(offer.type);
+  popupType.textContent = typeHabitation[advertisement.offer.type];
 
-  // Выведите количество гостей и комнат offer.rooms и offer.guests в блок .popup__text--capacity строкой вида {{offer.rooms}} комнаты для {{offer.guests}} гостей. Например, «2 комнаты для 3 гостей».
   const popupRoomsGuests = cardElement.querySelector('.popup__text--capacity');
-  popupRoomsGuests.textContent = '${offer.rooms} комнаты для ${offer.guests} гостей';
+  popupRoomsGuests.textContent = `${advertisement.offer.rooms} комнаты для ${advertisement.offer.guests} гостей`;
 
   //Заезд после {{offer.checkin}}, выезд до {{offer.checkout}}. Например, «Заезд после 14:00, выезд до 14:00».
-  const popupCheckinOut = cardElement.querySelector('.ppopup__text--time');
-  popupCheckinOut.textContent = 'Заезд после ${offer.checkin}, выезд до ${offer.checkout}';
+  const popupCheckinOut = cardElement.querySelector('.popup__text--time');
+  popupCheckinOut.textContent = `Заезд после ${advertisement.offer.checkin}, выезд до ${advertisement.offer.checkout}`;
 
   // В список .popup__features выведите все доступные удобства в объявлении.
+  const popupFeatures = cardElement.querySelector('.popup__features');
+  popupFeatures.textContent = advertisement.offer.features;
 
-  const popupDescription = element.querySelector('.popup__description');
-  popupDescription.textContent = offer.description;
+  const popupDescription = cardElement.querySelector('.popup__description');
+  popupDescription.textContent = advertisement.offer.description;
 
   // В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как атрибут src соответствующего изображения.
+  const popupPhotos = cardElement.querySelector('.popup__photos');
+  popupPhotos.textContent = advertisement.offer.photos;
 
-  const popupAvatar = element.querySelector('.popup__avatar');
-  popupAvatar.src = author.avatar;
+  const popupAvatar = cardElement.querySelector('.popup__avatar');
+  popupAvatar.src = advertisement.author.avatar;
 
   //отрисовать на карте
   insertAdvertisement.appendChild(cardElement);
